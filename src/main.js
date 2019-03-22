d24f7973f5e76adc0026f6f09a1c05e88af42d6a
xhhhy <1019690032@qq.com>
2019-03-22 14:31:44 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -20,8 +20,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  el: '#app',
+  render: h => h(App)
 })
